0c6feaae1af0c5eee13b21cf378807570fd955c7
kaisershbzt <pernezetsky@gmail.com>
2022-02-12 20:45:28 +0100

    idk

--- chat_application/src/components/MessageForm.jsx
+++ chat_application/src/components/MessageForm.jsx
@@ -1,10 +1,22 @@
+import { useState } from 'react';
+
 const MessageForm = () => {
+    const [value, setValue] = useState('');
     const handleSubmit = () => {
-        
+
+    }
+    const handleChange = () => {
+
     }
     return(
         <form className="messageForm" onSubmit={handleSubmit}>
-
+            <input 
+            className="message-input"
+            placeholder="Send a message"
+            value={value}
+            onChange={handleChange}
+            onSubmit={handleSubmit}
+            />
         </form>
     )
 }
